14579a133e586c94720d036faaf560bad6c71fa5
nicholejoser <nichole.joser@ricklee.com.ph>
2026-04-14 16:33:38 +0800

    initial commit message

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['192.168.16.142','110.93.79.226'],  
 };
 
 export default nextConfig;
