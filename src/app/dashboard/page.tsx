9f0ff6ba900c1d87d88d164967f2ab7d010dd59c
panisjr <ramelopanisjr.06@gmail.com>
2026-04-29 09:31:06 +0800

    removed unnecessary code

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -523,7 +523,7 @@ export default function Dashboard() {
                     Security Status
                   </span>
                   <p className="text-lg font-bold mt-1 text-emerald-600">
-                    Verified hahahahaha
+                    Verified
                   </p>
                 </div>
 
