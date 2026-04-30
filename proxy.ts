74abbbf8ba0023b80502e75b6e759ebd6cc2e8e7
panisjr <ramelopanisjr.06@gmail.com>
2026-04-30 09:44:11 +0800

    update system architecture

--- proxy.ts
+++ proxy.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function proxy(request: NextRequest) {
   const foodie = request.cookies.get("WTBkR2VWbFhNVFk9")?.value;
